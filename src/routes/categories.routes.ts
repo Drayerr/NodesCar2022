e68713f63592162d040e37f0806b94f24d81221e
Drayerr <gabrielgrv2015@outlook.com>
2022-08-31 10:55:45 -0300

    Fix list of categories using 'SINGLETON' instance.

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,15 @@
 import { Router } from 'express'
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository'
-import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService'
+import { createCategoryController } from '../modules/cars/useCases/createCategory'
+import { listCategoriesController } from '../modules/cars/useCases/listCategories'
 
 const categoriesRoutes = Router()
-const categoriesRepository = new CategoriesRepository()
 
 categoriesRoutes.post("/", (request, response) => {
-  const { name, description } = request.body
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository)
-
-  createCategoryService.execute({ name, description })
-
-  return response.status(201).send()
+  return createCategoryController.handle(request, response)
 })
 
 categoriesRoutes.get("/", (request, response) => {
-  const list = categoriesRepository.list()
-
-  return response.status(200).json(list)
+  return listCategoriesController.handle(request, response)
 })
 
 export { categoriesRoutes }
